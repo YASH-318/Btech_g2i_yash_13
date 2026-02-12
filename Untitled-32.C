f43b3efca7bc90023e4b9c0f17f1301b53d4b59b
YASH-318 <25116cn398@glbitm.ac.in>
2026-02-12 11:48:28 +0530

    Add files via upload

---
+++ Untitled-32.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ int main()
+ {
+    int num1,num2,num3;
+    printf("enter 3 number \n");
+    scanf("%d%d%d",&num1,&num2,&num3);
+    if (num1>num2 && num1>num3)
+    {
+        printf("greater number is %d",num1);
+    }
+    else if(num2>num1 &&num2>num3)
+    {
+        printf("greater number is %d",num2);
+    }
+    else
+    {
+        printf("greater number is %d",num3);
+    }
+    return 0;
+}
