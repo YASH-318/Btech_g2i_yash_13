e4d151b70de259b59225a94e8b805dfef1c54065
YASH-318 <25116cn398@glbitm.ac.in>
2026-02-19 11:49:39 +0530

    Add files via upload

---
+++ Untitled-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+char v ;
+printf("ENTER THE ALPHABET");
+scanf("%c",&v);
+if(v=='a'|| v=='e' ||v=='i'|| v=='o'|| v=='u'  ){
+    printf("the char is vowel %c \n");
+}else{
+    printf("the char is conconent %c \n");
+    
+}
+return 0;
+}
+h
+g0
